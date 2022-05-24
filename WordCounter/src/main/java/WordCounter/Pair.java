4ed5fe9049fe2aa9cf0879fe8b32d38a36984051
syafiqah-violet <nursyafiqah.sarifishak@gmail.com>
2022-05-24 17:09:53 +0800

    edit Pair

--- WordCounter/src/main/java/WordCounter/Pair.java
+++ WordCounter/src/main/java/WordCounter/Pair.java
@@ -6,8 +6,25 @@ package WordCounter;
 
 /**
  *
- * @author USER
+ * @author syafiqah-violet
+ * 
+ * A utility class for determining frequencies of words
  */
 public class Pair {
     
+    private String word;
+    private Integer frequency;
+
+    public Pair(String word, Integer frequency){
+        this.word = word;
+        this.frequency = frequency;
+    }
+
+    public Integer getFrequency() {
+        return frequency;
+    }
+
+    public String getWord() {
+        return word;
+    }   
 }
