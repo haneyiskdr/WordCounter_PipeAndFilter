5392c65648ed4d99d3854a0aeedca9657240abae
syafiqah-violet <nursyafiqah.sarifishak@gmail.com>
2022-05-24 16:59:45 +0800

    edit frequencySorter

--- WordCounter/src/main/java/WordCounter/FrequencySorter.java
+++ WordCounter/src/main/java/WordCounter/FrequencySorter.java
@@ -4,10 +4,23 @@
  */
 package WordCounter;
 
+import java.util.Comparator;
+
 /**
  *
- * @author USER
+ * @author syafiqah-violet
+ * 
+ *  Comparator class for sorting words by their frequency, and then by name if there is a tie
  */
-public class FrequencySorter {
+public class FrequencySorter implements Comparator<Pair> {
+    
+   //@Override
+    public int compare(Pair o1, Pair o2) {
+        int compareVal =  o2.getFrequency().compareTo(o1.getFrequency());
+        if (compareVal != 0){
+            return compareVal;
+        }
+        return o1.getWord().compareTo(o2.getWord());
+    }
     
 }
