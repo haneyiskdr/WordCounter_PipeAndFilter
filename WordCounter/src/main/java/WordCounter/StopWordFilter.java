b00f69e7562d713e7aac683113fb84a5ecad1fd4
marsyaaa <marsyaamelia00@gmail.com>
2022-06-07 11:46:01 +0800

    add stop words filter

--- WordCounter/src/main/java/WordCounter/StopWordFilter.java
+++ WordCounter/src/main/java/WordCounter/StopWordFilter.java
@@ -2,12 +2,109 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package WordCounter;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author USER
+ * @author marsyaaa
+ * 
+ * To filter out stop words from input text files
+ * 
  */
-public class StopWordFilter {
+public class StopWordFilter extends Filter {
     
+    private List<String> stopWords = new ArrayList();
+    private List<Long> times = new ArrayList<>();
+    private Long totalTime;
+
+    public StopWordFilter(Pipe in, Pipe out) {
+        super(in, out);
+    }
+
+    public Long getTotalTime() {
+        return totalTime;
+    }
+
+    public long getAvgTime() {
+        long total = 0;
+        
+        for (Long time: times) {
+            total += time;
+        }
+        
+        return (total / times.size()) / 100;
+    }
+
+    // To construct the stopwords list
+    public void setUp(String swfilename) {
+
+        // Try to read file, if cannot then throw exception
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(swfilename)));
+            List<String> words = new ArrayList<>();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                words.add(line);
+            }
+
+            //List<String> words = Files.readAllLines(Paths.get(getClass().getClassLoader().getResourceAsStream(filename)));
+            for (String word : words) {
+                String cleanedWord = word.replaceAll("[\\n\\t]", "");
+                stopWords.add(cleanedWord);
+            }
+ 
+        } catch (IOException ioe) {
+            System.out.println("Unable to read from stopwords.txt.");
+            ioe.printStackTrace();
+        }
+
+        System.out.println("Stop word filter setup has been completed.");
+        
+    }
+
+    // To filter out stop words from the input text file
+    public void run() {
+        
+        // System.out.println("Stop word filter has started.");
+
+        // Call stop word filter setup
+        if (stopWords.isEmpty()) {
+            this.setUp("stopwords.txt");
+        }
+
+        Instant startTime = Instant.now();
+
+        while (true) {
+            String word = getData();
+
+            if (word != Filter.POISON_PILL) {
+                if (word != null) {
+                    Instant start = Instant.now();
+                    
+                    if (!stopWords.contains(word)) {
+                        sendData(word);
+                    }
+                    
+                    Instant end = Instant.now();
+                    times.add(Duration.between(start, end).toNanos());
+                } else continue;
+            } else break;
+        }
+        
+        sendData(Filter.POISON_PILL);
+        Instant endTime = Instant.now();
+        totalTime = Duration.between(startTime, endTime).toMillis();
+        
+        System.out.println("Stop word filtering has been completed.");
+    }
 }
