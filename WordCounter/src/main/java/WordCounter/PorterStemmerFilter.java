5ed40e7c49aabe61506ad9c0c7fff20340d4c16a
SITI AISYAH ATIRAH <66213922+aisyahatirah@users.noreply.github.com>
2022-06-07 14:24:57 +0800

    add stemmer and porter stemmer filter

--- WordCounter/src/main/java/WordCounter/PorterStemmerFilter.java
+++ WordCounter/src/main/java/WordCounter/PorterStemmerFilter.java
@@ -1,13 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package WordCounter;
+package main;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * PorterStemmerFilter.java
  *
- * @author USER
+ * This class is designed to change words into their base forms
+ *      ie. remove all tense changes
+ * Utilizes the Stemmer.java class
+ *
  */
-public class PorterStemmerFilter {
-    
+public class PorterStemmerFilter extends Filter implements Runnable{
+
+    private List<Long> times = new ArrayList<>();
+    private Long totalTime;
+
+    public PorterStemmerFilter(Pipe in, Pipe out) {
+        super(in, out);
+    }
+
+    public Long getTotalTime() {
+        return totalTime;
+    }
+
+    public long getAvgTime(){
+        long sum = 0;
+        for (Long time: times){
+            sum += time;
+        }
+        return (sum / times.size()) / 100;
+    }
+
+    @Override
+    public void run() {
+        //System.out.println("PorterStemmerFilter started");
+        Instant totalStart = Instant.now();
+
+        while (true) {
+
+            String word = getData();
+
+            if (word == null ? Filter.POISON_PILL != null : !word.equals(Filter.POISON_PILL)) {
+                if (word != null) {
+                    Instant actionStart = Instant.now();
+                    Stemmer stemmer = new Stemmer();
+                    //System.out.println(word);
+                    for (int i = 0; i < word.length(); i++){
+                        stemmer.add(word.charAt(i));
+                    }
+                    stemmer.stem();
+                    sendData(stemmer.toString());
+                    Instant actionEnd = Instant.now();
+                    times.add(Duration.between(actionStart,actionEnd).toNanos());
+                } else continue;
+            } else break;
+        }
+        sendData(Filter.POISON_PILL);
+        Instant totalEnd = Instant.now();
+        totalTime = Duration.between(totalStart, totalEnd).toMillis();
+        System.out.println("PorterStemmerFilter finished");
+    }
 }
